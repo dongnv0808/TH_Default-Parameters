477440c985b8b87720689540e7f921163e1f2cef
dongnv0808 <yamaraja0808@gmail.com>
2022-06-01 21:45:02 +0700

    done

---
+++ default-parameters-example.js
@@ -0,0 +1,19 @@
+function multiply(a, b){
+    return a*b;
+}
+
+let num1 = multiply(5);
+console.log(num1);
+
+let num2 = multiply(4, 4);
+console.log(num2);
+
+
+class Geeks {
+    constructor(a, b = 2){
+        console.log(a*b);
+    }
+}
+
+let obj = new Geeks(5);
+let obj1 = new Geeks(5,4);
